07c6d17a0338d46c340e196719b7742dbf183d8b
soramikumo <sorao0324@gmail.com>
2023-06-05 00:06:15 +0900

    6/5

---
+++ src/compornetns/BlogPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function BlogPage() {
+  return (
+    <div className='container'>
+      <h1>BlogPage</h1>
+      <p>lorem100  </p>
+    </div>
+  )
+}
+
+export default BlogPage
